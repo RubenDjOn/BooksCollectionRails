34f7f65c8c4fecb0a67bc118c748eb5b0d19c93f
imacnu <imacnu@gmail.com>
2015-02-17 18:01:10 +0100

    edit and delete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,7 +21,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to books_path, notice: 'Book was successfully created.' }
+        format.html { redirect_to books_path, notice: 'Book ' + @book.title + ' was successfully created.' }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new }
@@ -31,9 +31,23 @@ class BooksController < ApplicationController
   end
 
   def update
+     respond_to do |format|
+      if @book.update(book_params)
+        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+        format.json { render :show, status: :ok, location: @book }
+      else
+        format.html { render :edit }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @book.destroy
+    respond_to do |format|
+      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
